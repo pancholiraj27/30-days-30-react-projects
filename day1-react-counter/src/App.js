2c6e22bf0eff2e1b2dca70888a730b7fff847a58
Raj Pancholi <75990574+pancholiraj27@users.noreply.github.com>
2024-01-11 13:06:42 +0530

    day 1 code added

---
+++ day1-react-counter/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div>30 Days 30 React Projects</div>
+        <div>day 2</div>
+      </nav>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
