29599c39c8fb1ce424ebf4d4a08143a2b04a294b
Raj Pancholi <75990574+pancholiraj27@users.noreply.github.com>
2024-01-16 14:04:10 +0530

    day 5

---
+++ day5-image-gallery/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Gallery from "./components/Gallery";
+
+function App() {
+  return (
+    <div className="container">
+      <nav className="navbar">
+        <div>30 Days 30 React Projects</div>
+        <div>day 5</div>
+      </nav>
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
